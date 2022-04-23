bf67092424529a0a853007d3848af68f1ae18d4b
Sheldon <sheldonaldridge@yahoo.com>
2022-04-24 00:01:05 +0200

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let email = document.querySelector(".email");
+let password = document.querySelector(".password");
+let checkbox = document.querySelector(".checkbox");
+
+checkbox.addEventListener('click', event =>{
+
+    if(checkbox)
+    {
+        
+    }
+})
