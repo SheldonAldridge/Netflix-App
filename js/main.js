533b7d1129210c28c46f2ebd26bc2792cf905b25
Sheldon <sheldonaldridge@yahoo.com>
2022-04-24 18:53:39 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,39 @@
 let email = document.querySelector(".email");
 let password = document.querySelector(".password");
-let checkbox = document.querySelector(".checkbox");
+let checkbox= document.querySelector(".checkbox")
+
+//Storing users email and password
+function store()
+{
+    localStorage.setItem('email',email.value)
+    localStorage.setItem('password', password.value)
+}
+
+function checkLogin()
+{
+    //get set stored data
+    let emailStored = localStorage.getItem('email')
+    let passwordStored = localStorage.getItem('password')
+
+    //data entered from login-form
+    let emailInput = document.querySelector('.email').value
+    let passwordInput = document.querySelector('.password').value
+
+    if(emailInput != emailStored)
+    {
+       let eError = document.querySelector(".email-error-message")
+       eError.innerHTML = "Please enter valid email address"
+    }
+
+    if(passwordInput != passwordStored)
+    {
+        let pError = document.querySelector(".password-error-message")
+        pError.innerHTML = "Please enter password associated with email address"
+    }
+
+}
+
 
 checkbox.addEventListener('click', event =>{
-
-    if(checkbox)
-    {
-        
-    }
+    
 })
