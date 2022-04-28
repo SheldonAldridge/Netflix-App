c6b479d9d3895b88e924aebb7926a16575feea04
Sheldon <sheldonaldridge@yahoo.com>
2022-04-28 15:14:40 +0200

    Update moviesClasses.js
    added Duration for the moviesList constructor

--- js/moviesClasses.js
+++ js/moviesClasses.js
@@ -1,12 +1,13 @@
 export class MovieList{
 
-    constructor(id , namesOfMovies, genre, comingSoon, availableDate, thumbnail, preview){
+    constructor(id , namesOfMovies, genre, comingSoon, availableDate, duration, thumbnail, preview){
 
         this.id = id;
         this.name = namesOfMovies;
         this.genre = genre;
         this.comingSoon = comingSoon;
         this.availDate = availableDate;
+        this.duration = duration;
         this.thumbnail = thumbnail;
         this.preview = preview;
     }
