2b557bd40f10146b4b87cd5ebe31df455147721f
Sheldon <sheldonaldridge@yahoo.com>
2022-05-05 21:05:28 +0200

    Update home.js

--- js/home.js
+++ js/home.js
@@ -9,8 +9,8 @@ Vue.createApp({
       movies:[
         new movie("#6507", "The Adam Project", "Sci-Fi", "false", "9 March 2022", "../images/The Adam Project.jpg"),
         new movie("#7770", "Here Comes The Boom", "Comedy", "false", "12 October 2012", "../images/Here Comes The Boom.jpg"),
-        new movie("#2385", "IZombie", "Crime TV Shows", "false", "17 March 2015", "../images/I Zombie.jpg"),
-        new movie("#2892", "The Witcher", "Fantasy TV Shows", "false", "20 December 2019", "../images/The Witcher.jpg"),
+        new movie("#2385", "IZombie", "Crime", "false", "17 March 2015", "../images/I Zombie.jpg"),
+        new movie("#2892", "The Witcher", "Fantasy", "false", "20 December 2019", "../images/The Witcher.jpg"),
         new movie("#8679", "The Fighter", "Drama", "false", "25 February 2011", "../images/The Fighter.jpg")
       ],
 
@@ -35,6 +35,25 @@ Vue.createApp({
     },
   },
 
+  comedy(){
+    return this.movies.filter((movie) =>{
+      return movie.genre === "Comedy" && !movie.comingSoon;
+    });
+  },
+
+  drama(){
+    return this.movies.filter((movie) =>{
+      return movie.genre === "Drama" && !movie.comingSoon;
+    });
+  },
+
+  fantasy(){
+    return this.movies.filter((movie) =>{
+      return movie.genre === "Fantasy" && !movie.comingSoon;
+    });
+  },
+
+  
   methods:{
     watchList(index){
       if(!localStorage.getItem(WATCHLIST_KEY)){
@@ -43,14 +62,13 @@ Vue.createApp({
         localStorage.setItem(WATCHLIST_KEY, JSON.stringify(watchList));
       }
       else{
-        let watchList = JSON.parse(
-          localStorage.getItem(WATCHLIST_KEY)
-        );
+        let watchList = JSON.parse(localStorage.getItem(WATCHLIST_KEY));
 
         watchList.push(this.availMovies[index]);
         localStorage.setItem(WATCHLIST_KEY, JSON.stringify(watchList));
     }
   },
+},
 
   watchList(){
     window.location.href = "../pages/watchList.html";
