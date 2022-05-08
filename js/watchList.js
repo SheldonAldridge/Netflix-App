e9dce2845cba9f5b2c3a29ef5fcdaa8774c6a057
Sheldon <sheldonaldridge@yahoo.com>
2022-05-08 18:19:06 +0200

    update watchlist function

--- js/watchList.js
+++ js/watchList.js
@@ -7,16 +7,48 @@ Vue.createApp({
             users:[],
             username: "",
             search:"",
-            watchList:[],
+            myList:[],
         };
     },
 
     methods:{
         removeMovieList(id){
-            this.watchList = JSON.parse(
-                localStorage.getItem(WATCHLIST_KEY)
-            );
-            this.
-        }
-    }
-})
+            this.myList = JSON.parse(localStorage.getItem(WATCHLIST_KEY));
+        
+            this.myList = this.myList.filter(function (movie){
+            return movie.id != id;
+            });
+
+            localStorage.setItem(WATCHLIST_KEY,
+            JSON.stringify(this.myList));
+        },
+
+        home(){
+            window.location.href="../pages/home.html";
+        },
+        
+        logout(){
+            localStorage.removeItem(USER_SIGNED_IN_KEY);
+            window.location.href = "../index.html";
+        },
+    },
+
+    computed: {
+        myListFiltered() {
+          return this.myList.filter((movie) => {
+            return movie.name.toLowerCase().includes(this.search.toLowerCase());
+          });
+        },  
+      },
+
+      mounted(){
+          this.users = JSON.parse(localStorage.getItem(USER_SIGNED_IN_KEY));
+          this.username = this.users[0].username;
+
+          if(!localStorage.getItem(WATCHLIST_KEY)){
+              let listArray = [];
+              localStorage.setItem(WATCHLIST_KEY, JSON.stringify(listArray))
+          }
+          this.watchList = JSON.parse(localStorage.getItem(WATCHLIST_KEY))
+      },
+}).mount("#netflixapp")
